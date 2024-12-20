9d36d78bcf555271f9d1ed54bda176a44c299387
MariamGhanim <mariamghanimcs@gmail.com>
2024-12-20 19:03:12 +0200

    coupon

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.route.js';
 import productRoutes from './routes/product.route.js';
+import couponRoutes from "./routes/coupon.route.js";
 
 import { connectDB } from './lib/db.js';
 
@@ -17,6 +18,7 @@ app.use(cookieParser());
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
+app.use("/api/coupons", couponRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
